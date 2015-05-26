64b39eb45b1273ba50444ce1a925f3cf3cc71146
Josh Bronson <Josh Bronson>
2015-05-26 10:29:57 -0500

    copying validity.notAfter to "expires"
    This appears necessary at the moment to appease the EZBake frontend.

--- security/services/security-service/src/main/java/ezbake/security/service/ThriftTokenJSONProvider.java
+++ security/services/security-service/src/main/java/ezbake/security/service/ThriftTokenJSONProvider.java
@@ -17,13 +17,17 @@ package ezbake.security.service;
 import ezbake.base.thrift.EzSecurityToken;
 import ezbake.base.thrift.EzSecurityTokenJson;
 import ezbake.security.common.core.TokenJSONProvider;
+import ezbake.base.thrift.EzSecurityTokenException;
 import ezbake.crypto.PKeyCrypto;
 import org.apache.thrift.TException;
 import org.apache.thrift.TSerializer;
 import org.apache.thrift.protocol.TSimpleJSONProtocol;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -33,15 +37,33 @@ import java.io.UnsupportedEncodingException;
  */
 public class ThriftTokenJSONProvider implements TokenJSONProvider {
     private static final Logger LOGGER = LoggerFactory.getLogger(ThriftTokenJSONProvider.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Override
     public EzSecurityTokenJson getTokenJSON(EzSecurityToken token, PKeyCrypto signer)
             throws TException, UnsupportedEncodingException {
 
         EzSecurityTokenJson json = new EzSecurityTokenJson();
-        json.setJson(new String(new TSerializer(new TSimpleJSONProtocol.Factory()).serialize(token), "UTF-8"));
+        String jsonString = new String(new TSerializer(new TSimpleJSONProtocol.Factory()).serialize(token), "UTF-8");
+        
         json.setSignature("");
-        return json;
-    }
 
+        if (token.getValidity() == null) {
+            throw new EzSecurityTokenException("no validity caveats for token: " + json);
+        }
+
+        ObjectNode o = null;
+        try {
+            o = ((ObjectNode)MAPPER.readTree(jsonString));
+            // Copy validity.notAfter to expires so that this can be
+            // cached and reused correctly by the frontend.
+            o.put("expires", token.getValidity().getNotAfter());
+
+            json.setJson(MAPPER.writeValueAsString(o));
+        } catch (IOException e) {
+            LOGGER.error("trouble modifying the json string that was just created: {}", json, e);
+        } finally {
+            return json;
+        }
+    }
 }
